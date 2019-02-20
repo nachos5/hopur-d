fbebefaf65c9cddcb63875960ce853e300117eb2
MadSuperSheep <hjaltigeir@gmail.com>
2019-02-20 13:53:57 +0000

    auka commit

---
+++ src/main/controllers/MenuBarController.java
@@ -0,0 +1,39 @@
+package main.controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuBarController {
+    @FXML
+    public AnchorPane mainAnchor;
+    @FXML
+    public Menu fileItem;
+    @FXML
+    public MenuItem openItem;
+    @FXML
+    public MenuItem saveItem;
+    @FXML
+    public MenuItem quitItem;
+    @FXML
+    public Menu settingsItem;
+    @FXML
+    public Menu languageItem;
+    @FXML
+    public MenuItem icelandic;
+    @FXML
+    public MenuItem english;
+
+    @FXML
+    private void quitProgram(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    private void temp(ActionEvent event) {
+        System.out.println("Test item");
+    }
+}
